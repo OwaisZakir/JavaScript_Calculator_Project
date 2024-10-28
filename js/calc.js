8f7c9ce2f385f21ca7b3041528a2d0626a2761dc
OwaisZakir <owaiszakir88@gmial.com>
2024-10-28 08:34:03 -0700

    Done

---
+++ js/calc.js
@@ -0,0 +1,42 @@
+let display = document.querySelector("#userDisplay");
+let buttons = document.querySelectorAll("button");
+let arrButton = Array.from(buttons);
+let string = "";
+arrButton.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    if (btn.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    } else if (btn.innerHTML == "AC") {
+      (string = ""), (display.value = string);
+    } else if (btn.innerHTML == "=") {
+      string = eval(string);
+      if (string == undefined) {
+        alert("Please Enter The Number First");
+        string = "";
+      } else {
+        display.value = string;
+      }
+    } else if (btn.innerHTML === "**") {
+      string = string ** 2;
+      display.value = string;
+      string = "";
+    } else {
+      string += btn.innerHTML;
+      switch (string) {
+        case ".":
+        case "0":
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "**":
+          alert(`You Can't Enter The ${btn.innerHTML} At First`);
+          string = "";
+          break;
+        default:
+          display.value = string;
+      }
+    }
+  });
+});
